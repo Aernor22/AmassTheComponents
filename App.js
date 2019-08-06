494823c415218df1387acd9f27ec5c823fb4a920
Aernor22 <bugada.lucas@gmail.com>
2019-08-06 10:26:03 -0300

    update

--- App.js
+++ App.js
@@ -8,70 +8,25 @@
 
 import React, {Fragment} from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
   StatusBar,
 } from 'react-native';
-import { StackNavigator } from 'react-navigation';
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import Main from "./src/activities/Main";
+import Camera from "./src/activities/Camera";
 
-import Main from "./src/activities/Main"
+const AppNavigator = createStackNavigator({
+  Home: Main,
+  Camera: Camera
+  
+});
+const AppContainer = createAppContainer(AppNavigator);
 
 const App = () => {
   return (
     <Fragment>
       <StatusBar barStyle="dark-content" />
-      <Main></Main>
+      <AppContainer/>
     </Fragment>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
 export default App;
