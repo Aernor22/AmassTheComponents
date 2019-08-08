913ec4c7a62f65eee2dd397d053721ad65e0b4bc
Lucas dos Santos Bugada <bugada.lucas@gmail.com>
2019-08-08 14:43:09 -0300

    tesseract INIT

--- src/activities/Camera.js
+++ src/activities/Camera.js
@@ -1,12 +1,27 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable quotes */
 import React, { Component } from "react";
 import { RNCamera } from "react-native-camera";
-import { View, Text } from "react-native";
+import RNTesseractOcr from "react-native-tesseract-ocr";
+import { View } from "react-native";
+
+const PICTURE_OPTIONS = {
+  quality: 1,
+  fixOrientation: true,
+  forceUpOrientation: true,
+};
+const tessOptions = {
+  whitelist: null,
+  blacklist: null,
+};
 
 export default class Camera extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+  state = {
+    loading: false,
+    image: null,
+    error: null,
+    visionResp: [],
+  };
 
   render() {
     return (
@@ -23,8 +38,62 @@ export default class Camera extends Component {
           permissionDialogMessage={
             "We need your permission to use your camera phone"
           }
-        />
+        />{" "}
       </View>
     );
   }
+
+  reset(error = "OTHER") {
+    this.setState(
+      {
+        loading: false,
+        image: null,
+        error,
+      },
+      () => {
+        // setTimeout(() => this.camera.startPreview(), 500);
+      }
+    );
+  }
+
+  takePicture = async camera => {
+    this.setState({
+      loading: true,
+    });
+    try {
+      const data = await camera.takePictureAsync(PICTURE_OPTIONS);
+      if (!data.uri) {
+        throw "OTHER";
+      }
+      this.setState(
+        {
+          image: data.uri,
+        },
+        () => {
+          console.log(data.uri);
+          this.processImageMagic(data.uri);
+        }
+      );
+    } catch (e) {
+      console.warn(e);
+      this.reset(e);
+    }
+  };
+
+  processImageMagic = async uri => {
+    RNTesseractOcr.recognize(uri, "LANG_CUSTOM", tessOptions)
+      .then(result => {
+        this.setState({
+          isLoading: false,
+          extractedText: result,
+        });
+      })
+      .catch(err => {
+        this.setState({
+          hasErrored: true,
+          errorMessage: err.message,
+        });
+        throw "FAILED TO DETECT";
+      });
+  };
 }
--- src/activities/Main.js
+++ src/activities/Main.js
@@ -1,36 +1,42 @@
-import ActionButton from 'react-native-action-button';
-import React, { Component } from 'react';
-import Icon from 'react-native-vector-icons/AntDesign';
-import {
-    View,
-    StyleSheet
-  } from 'react-native';
+import ActionButton from "react-native-action-button";
+import React, { Component } from "react";
+import Icon from "react-native-vector-icons/AntDesign";
+import { View, StyleSheet } from "react-native";
 class Main extends Component {
-    state = { 
-
-     }
-    render() {
-        return (
-            <View>
-                <ActionButton onPress={() => { console.log("hi")}} position="left" icon={<Icon name="export2" style={styles.actionButtonIcon}/>}/>
-                <ActionButton 
-                  position="center" 
-                  icon={<Icon name="camerao" style={styles.actionButtonIcon}/>} 
-                  offsetY={80}                   
-                  onPress={() => { console.log("bye")}}
-                  />
-                <ActionButton position="right" icon={<Icon name="eyeo" style={styles.actionButtonIcon}/>}/>
-            </View>
-        );
-    }
+  state = {};
+  render() {
+    return (
+      <View>
+        <ActionButton
+          onPress={() => {
+            console.log("hi");
+          }}
+          position="left"
+          icon={<Icon name="export2" style={styles.actionButtonIcon} />}
+        />
+        <ActionButton
+          position="center"
+          icon={<Icon name="camerao" style={styles.actionButtonIcon} />}
+          offsetY={80}
+          onPress={() => {
+            console.log("bye");
+          }}
+        />
+        <ActionButton
+          position="right"
+          icon={<Icon name="eyeo" style={styles.actionButtonIcon} />}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    actionButtonIcon: {
-      fontSize: 20,
-      height: 22,
-      color: 'white',
-    },
-  });
+  actionButtonIcon: {
+    fontSize: 20,
+    height: 22,
+    color: "white"
+  }
+});
 
 export default Main;
