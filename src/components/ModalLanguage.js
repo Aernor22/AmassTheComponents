c4fac3ab15fb79d174960d855a83edb5cd474d99
Aernor22 <bugada.lucas@gmail.com>
2019-09-06 11:05:13 -0300

    ModalLanguage: Styles

--- src/components/ModalLanguage.js
+++ src/components/ModalLanguage.js
@@ -1,21 +1,21 @@
 /* eslint-disable prettier/prettier */
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Modal, Dimensions } from 'react-native';
 import Carousel from 'react-native-snap-carousel';
 
 export default class ModalLanguage extends Component {
-    state ={
-        entries:["English",
-         "Chinese Simplified", 
-         "Chinese Traditional", 
-         "French", 
-         "German", 
-         "Italian", 
-         "Japanese", 
-         "Korean", 
-         "Portuguese", 
-         "Russian",
-         "Spanish"]
+    state = {
+        entries: ["English",
+            "Chinese Simplified",
+            "Chinese Traditional",
+            "French",
+            "German",
+            "Italian",
+            "Japanese",
+            "Korean",
+            "Portuguese",
+            "Russian",
+            "Spanish"]
     }
     _renderItem({ item, index }) {
         return (
@@ -27,16 +27,34 @@ export default class ModalLanguage extends Component {
 
     render() {
         return (
-            <View>
-                <Text> Text recognized! Please select a language: </Text>
-                <Carousel
-                    ref={(c) => { this._carousel = c; }}
-                    data={this.state.entries}
-                    renderItem={this._renderItem}
-                    sliderWidth={60}
-                    itemWidth={40}
-                    layout={'default'}
-                />
+            <View style={{ marginTop: 22 }}>
+                <Modal
+                    animationType="slide"
+                    transparent={true}>
+                    <View style={{
+                        flex: 1,
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        backgroundColor:'#00000080'
+                    }}>
+                        <View style={{
+                            width: Dimensions.get('window').width * 0.5,
+                            height: Dimensions.get('window').height * 0.5,
+                            backgroundColor: '#fff', padding: 20
+                        }}>
+                            <Text> Text recognized! Please select a language: </Text>
+                            <Carousel
+                                ref={(c) => { this._carousel = c; }}
+                                data={this.state.entries}
+                                renderItem={this._renderItem}
+                                sliderWidth={60}
+                                itemWidth={40}
+                                layout={'default'}
+                            />
+                        </View>
+                    </View>
+                </Modal>
             </View>
         )
     }
